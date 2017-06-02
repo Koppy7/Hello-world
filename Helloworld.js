3c7bb30de728c3d15a2693436a74b98b93b6861a
LeopoldDigitalAndYou <leo@garage56.fr>
2017-06-02 15:15:44 +0200

    initial commit

--- Helloworld.js
+++ Helloworld.js
@@ -1,5 +1,10 @@
-var count = 1;
-
-console.log("count =", count);
-
+(function () {
+		var count = 1;
+		document.getElementById("inFunction").innerHTML = "Nous sommes ici à l'intérieur de la fonction, la valeur de count est égal à : " + count + " :)";
+			
+})();
+//J'espere que tu ne pourras pas toucher à ma variable.
+console.log(count);
 // A partir d'ici je ne veux plus pouvoir faire un count = 2;
+count = 2
+console.log(count);
--- Zebi Node/Server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
-    "use strict";
-    res.writeHead(200);
-    res.end('Hello World !');
-});
-server.listen(8081);
