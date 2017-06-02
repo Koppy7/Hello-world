ac613b749788b1c43b547915ca908a2aaf0cb8af
LeopoldDigitalAndYou <leo@garage56.fr>
2017-06-02 15:23:19 +0200

    Second commit

--- Helloworld.js
+++ Helloworld.js
@@ -1,6 +1,6 @@
 (function () {
 		var count = 1;
-		document.getElementById("inFunction").innerHTML = "Nous sommes ici à l'intérieur de la fonction, la valeur de count est égal à : " + count + " :)";
+		console.log("Nous sommes ici à l'intérieur de la fonction, la valeur de count est égal à : " + count + " :)");
 			
 })();
 //J'espere que tu ne pourras pas toucher à ma variable.
@@ -8,3 +8,8 @@ console.log(count);
 // A partir d'ici je ne veux plus pouvoir faire un count = 2;
 count = 2
 console.log(count);
+
+
+
+
+
--- spawned.js
+++
@@ -1,8 +0,0 @@
-setInterval(function() {
-    process.stdout.write('.');
-}, 1000);
-
-process.stdin.on('data', function () {
-    console.log('Stopped by parent');
-    process.exit();
-});
