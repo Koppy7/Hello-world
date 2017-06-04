14cb021249a8c75196fa0c08ac27a851cdabbcee
Xavier Boubert <xavier@boubert.fr>
2017-06-04 21:52:17 +0200

    true hello world

--- Helloworld.js
+++ Helloworld.js
@@ -1,15 +1,5 @@
 (function () {
-		var count = 1;
-		console.log("Nous sommes ici à l'intérieur de la fonction, la valeur de count est égal à : " + count + " :)");
-			
+  'use strict';
+	
+  console.log('Hello World');
 })();
-//J'espere que tu ne pourras pas toucher à ma variable.
-console.log(count);
-// A partir d'ici je ne veux plus pouvoir faire un count = 2;
-count = 2
-console.log(count);
-
-
-
-
-
