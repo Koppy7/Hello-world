22bfc76d5c51bbc71d8139f01fbb001c1eb32585
LeopoldDigitalAndYou <leo@garage56.fr>
2017-06-04 22:07:47 +0200

    variable types

--- Helloworld.js
+++ Helloworld.js
@@ -1,5 +1,17 @@
 (function () {
   'use strict';
-	
   console.log('Hello World');
+
+var toto = 1;
+var tota = 'A';
+var toti = "ABZ";
+var totu = (function(){})();
+var tote = function(){};
+var toty = 1.50;
+var tott = 10%;
+var totta = Object.Create('lol');
+
+
+
+
 })();
