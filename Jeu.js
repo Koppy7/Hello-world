370a74548ad8c29d7f3ae5bfeddc9552a902dfb6
Xavier Boubert <xavier@boubert.fr>
2017-06-11 21:33:26 +0200

    refactor Jeu.js to be more object oriented

--- Jeu.js
+++ Jeu.js
@@ -1,28 +1,67 @@
 function Perso (Name, Gender, Alias, From, HP, Weapon, weaponPower, criticalWeapon, Master) {
-    this.Name = Name;
-    this.Gender = Gender;
-    this.Alias = Alias;
-    this.From = From;
-    this.HP = HP;
-    this.Weapon = Weapon;
+    var _this = this;
+
+    this.name = Name;
+    this.gender = Gender;
+    this.alias = Alias;
+    this.from = From;
+    this.hp = HP;
+    this.weapon = Weapon;
     this.weaponPower = weaponPower;
     this.criticalWeapon = criticalWeapon;
-    this.Master = Master;
-}
-    
-function decrire(obj) {
-    var description = "Nom : " + obj.Name + " \nAussi connu sous l'alias de " + obj.Alias + " \nil nous vient tout droit de " + 
-    obj.From + " il possède " + obj.HP + " points de vie !\nIl a décidé de se battre avec : " + obj.Weapon + " ! Son pouvoir est de : " + obj.weaponPower + "\nson Maitre trainer est : " 
-     + obj.Master + " !\n\n"
+    this.master = Master;
 
-                if (obj.Gender == "Femme") {
-                    description = "Nom : " + obj.Name + "\nAussi connue sous le pseudo de " + obj.Alias + "\nelle nous vient tout droit de " +
-                    obj.From + " elle possède " + obj.HP + " points de vie !\nIl a décidé de se battre avec : " + obj.Weapon + " ! Son pouvoir est de : " + obj.weaponPower + "\nSon Maitre trainer est : "
-                    + obj.Master + " !\n\n";
-                }
+    this.describe = function() {
+        var texts = {
+            MALE:
+                "[{{name}}] ({{hp}}hp)" +
+                "\nAussi connu sous le nom de {{alias}}" +
+                "\nIl nous vient tout droit de {{from}}" +
+                "\nIl a décidé de se battre avec {{weapon}}" +
+                "\nSon pouvoir est de {{weaponPower}}" +
+                "\nSon Maitre trainer est \"{{master}}\"",
+            FEMALE:
+                "[{{name}}] ({{hp}}hp)" +
+                "\nAussi connue sous le nom de {{alias}}" +
+                "\nElle nous vient tout droit de {{from}}" +
+                "\nElle a décidé de se battre avec {{weapon}}" +
+                "\nSon pouvoir est de {{weaponPower}}" +
+                "\nSon Maitre trainer est \"{{master}}\""
+        };
 
-            return description;
+        var text = texts[_this.gender],
+            key = null;
+
+        for (key in _this) {
+            if (_this.hasOwnProperty(key)) {
+                text = text.replace(new RegExp('{{' + key + '}}', 'g'), _this[key]);
+            }
+        }
+
+        return text;
+    };
 }
+
+Perso.GENDERS = {
+    MALE: "MALE",
+    FEMALE: "FEMALE"
+};
+
+// You can remove this
+
+// function decrire(obj) {
+//     var description = "Nom : " + obj.Name + " \nAussi connu sous l'alias de " + obj.Alias + " \nil nous vient tout droit de " +
+//     obj.From + " il possède " + obj.HP + " points de vie !\nIl a décidé de se battre avec : " + obj.Weapon + " ! Son pouvoir est de : " + obj.weaponPower + "\nson Maitre trainer est : "
+//      + obj.Master + " !\n\n"
+
+//                 if (obj.Gender == "Femme") {
+//                     description = "Nom : " + obj.Name + "\nAussi connue sous le pseudo de " + obj.Alias + "\nelle nous vient tout droit de " +
+//                     obj.From + " elle possède " + obj.HP + " points de vie !\nIl a décidé de se battre avec : " + obj.Weapon + " ! Son pouvoir est de : " + obj.weaponPower + "\nSon Maitre trainer est : "
+//                     + obj.Master + " !\n\n";
+//                 }
+
+//             return description;
+// }
 
 annonceArene = {
     bienvenue1: function() {
@@ -53,7 +92,7 @@ fight = {
             return Garde;
     },
     attaque: function(obj, adversaire) {
-        
+
         var attaque = obj.Name + " décide d'attaquer la première ! "
         var touch = obj.Name + " a touché sa cible ! Quel coup ! " + adversaire.Name + " perd " + obj.weaponPower + " HP !";
         var criticalTouch = obj.Name + " vient d'infliger un coup critique ! Exceptionnel ! " + adversaire.Name + " perd " + obj.criticalWeapon + " HP !";
@@ -64,21 +103,21 @@ fight = {
         if (obj.Gender == "Homme") {
                 attaque = obj.Name + " décide d'attaquer le premier ! ";
             }
-        
+
         if(echecCritique == 6) {
-                
+
                     return noTouch;
 
             } else if (coupCritique == 18) {
-                
+
                 adversaire.HP -= obj.criticalWeapon;
-                
+
                     return criticalTouch;
-           
+
              } else {
-            
+
                 adversaire.HP -= obj.weaponPower;
-            
+
                     return touch;
             }
         console.log(attaque);
@@ -92,29 +131,29 @@ fight = {
         var noTouch = obj.Name + " a raté son coup ! Quel dommage ! ";
         var echecCritique = Math.floor((Math.random() * 10) + 1);
         var coupCritique = Math.floor((Math.random() * 20) + 10);
-        
+
 
         console.log(attaque);
 
             if(echecCritique == 6) {
-        
+
                     return noTouch;
                 } else if (coupCritique == 18) {
-            
+
                     adversaire.HP -= obj.criticalWeapon;
-            
+
                         return criticalTouch;
-           
+
                 } else {
-                
+
                      adversaire.HP -= obj.weaponPower;
-                
+
                          return touch;
-            
+
         }
 
     }
-    
+
 }
 
 ambiance = {
@@ -125,16 +164,16 @@ ambiance = {
             } else {
             console.log(obj.Name + " se sent bien dans ce combat, il lui reste " + obj.HP + " HP !");
              }
-        
+
                 if (adversaire.HP < 21) {
-                       
-                        console.log("Ce n'est pas le cas pour son adversaire " + adversaire.Name + " qui possède maintenant " + adversaire.HP + " HP\n" + adversaire.Name + 
+
+                        console.log("Ce n'est pas le cas pour son adversaire " + adversaire.Name + " qui possède maintenant " + adversaire.HP + " HP\n" + adversaire.Name +
                         " décide d'abattre sa dernière carte, c'est le moment...\n");
-                
+
                 } else {
-                 
+
                     console.log(adversaire.Name + " ne se sent pas bien mais il n'abandonnera pas, c'est un combat à mort ! il lui reste " + adversaire.HP + " HP !\n");
-                
+
             }
         } else {
             if (obj.Gender == "Homme") {
@@ -151,9 +190,9 @@ ambiance = {
     }
 }
 
-var Travis = new Perso("Travis", "Homme", "Guardian Angel", "Independenza", 250, "Light Spirit", 60, 90, "-The W-");
-var Kaïna = new Perso("Kaïna", "Femme", "Fatal Smooth", "Kanazawa", 140, "Telescopic Katana", 30, 50, "Musashi Miyamoto");
-var Ryan = new Perso("Ryan", "Homme", "Fucking Casu", "Call of Duty", 50, "Shrill Voice", 15, 50, "'Only me can judge me'");
+var Travis = new Perso("Travis", Perso.GENDERS.MALE, "Guardian Angel", "Independenza", 250, "Light Spirit", 60, 90, "-The W-");
+var Kaïna = new Perso("Kaïna", Perso.GENDERS.FEMALE, "Fatal Smooth", "Kanazawa", 140, "Telescopic Katana", 30, 50, "Musashi Miyamoto");
+var Ryan = new Perso("Ryan", Perso.GENDERS.MALE, "Fucking Casu", "Call of Duty", 50, "Shrill Voice", 15, 50, "'Only me can judge me'");
 
 
  var Personnages = [];
@@ -162,15 +201,18 @@ var Ryan = new Perso("Ryan", "Homme", "Fucking Casu", "Call of Duty", 50, "Shril
  Personnages.push(Ryan);
 
  function decrireAllPerso() {
-        for (var i = 0; i < Personnages.length; i++){
-             var descriptionAll = "- Nom : " + Personnages[i].Name + " Aussi connu sous l'alias de " + Personnages[i].Alias + " \nil nous vient tout droit de " + 
-             Personnages[i].From + " il possède " + Personnages[i].HP + " points de vie !\n'DECLAREZ VOTRE ARME COMBATTANT' '" + Personnages[i].Weapon + " ! son pouvoir est de " + Personnages[i].weaponPower + "' 'très bien.' \nson Maitre trainer est : " 
-             + Personnages[i].Master + " !\n";
-             
-             console.log(descriptionAll);
-            }
+    for (var i = 0; i < Personnages.length; i++){
+         var descriptionAll = "- Nom : " + Personnages[i].Name + " Aussi connu sous l'alias de " + Personnages[i].Alias + " \nil nous vient tout droit de " +
+         Personnages[i].From + " il possède " + Personnages[i].HP + " points de vie !\n'DECLAREZ VOTRE ARME COMBATTANT' '" + Personnages[i].Weapon + " ! son pouvoir est de " + Personnages[i].weaponPower + "' 'très bien.' \nson Maitre trainer est : "
+         + Personnages[i].Master + " !\n";
+
+         console.log(descriptionAll);
         }
- 
+    }
+}
+
+console.log(Travis.describe());
+
 
  //console.log(annonceArene.bienvenue1());
  //console.log(decrire(Kaïna));
